24dc7198bb94aa176a747d6b8f4ef0dddfb6c257
Nicholas Reynaldo <nexilios@gmail.com>
2024-04-25 19:31:12 +0700

    Fixed alert message after booking not showing properly

--- public/js/index.js
+++ public/js/index.js
@@ -103,5 +103,5 @@ if (bookBtn)
     bookTour(tourId);
   });
 
-const alertMessage = document.querySelector('.body').dataset.alert;
-if (alertMessage) showAlert('success', alertMessage, 20);
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 15);
